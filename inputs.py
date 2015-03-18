259fedb1a4496173004e3b44f742f30c472a9ca1
Garrett Everding <garrett.everding92@gmail.com>
2015-03-17 20:41:59 -0400

    improvements to memberhsip functions to be more realistic

--- inputs.py
+++ inputs.py
@@ -9,23 +9,23 @@ from fuzzy.set.Trapez import Trapez
 # PAy
 def generate_pay(bad_low, bad_high, ok_low, ok_high, good_low, good_high ):
     pay_bad = Polygon()
-    pay_bad.add(x = 0, y= 0.0)
-    pay_bad.add(x = bad_low, y= 0.0)
+    pay_bad.add(x = max(bad_low - 5, 0), y= 0.0)
+    pay_bad.add(x = bad_low, y= 1.0)
     pay_bad.add(x = bad_high, y= 1.0)
-    pay_bad.add(x = ok_low + 5, y= 0.0)
+    pay_bad.add(x = bad_high + 5, y= 0.0)
 
     pay_ok = Polygon()
     pay_ok.add(x =  ok_low - 5 , y= 0.0)
     pay_ok.add(x =  ok_low, y= 1.0)
     pay_ok.add(x =  ok_high, y= 1.0)
-    pay_ok.add(x =  ok_high + 10, y= 0.0)
+    pay_ok.add(x =  ok_high + 20, y= 0.0)
 
     pay_good = Polygon()
-    pay_good.add(x =  ok_high, y= 0.0)
+    pay_good.add(x =  good_low - 10, y= 0.0)
     pay_good.add(x =  good_low, y= 1.0)
     pay_good.add(x =  good_high, y= 1.0)
     # everything over good_high is still good
-    pay_good.add(x =  good_high + 50 , y= 1.0)
+    # pay_good.add(x =  good_high + 50 , y= 1.0)
 
     return {
         "bad": pay_bad,
@@ -35,13 +35,13 @@ def generate_pay(bad_low, bad_high, ok_low, ok_high, good_low, good_high ):
 
 def generate_rep(low, high):
     low_rep = Polygon()
+    low_rep.add(x = 0, y = 0.0)
     low_rep.add(x = 0, y = 1.0)
     low_rep.add(x = low, y = 1.0)
-    #
-    low_rep.add(x = high + 2, y = 0.0)
+    low_rep.add(x = low + 1, y = 0.0)
 
     high_rep = Polygon()
-    high_rep.add(x = high - 1, y= 0.0)
+    high_rep.add(x = high - 3, y= 0.0)
     high_rep.add(x = high, y = 1.0)
     high_rep.add(x = 10, y = 1.0)
 
@@ -53,9 +53,12 @@ def generate_rep(low, high):
 def generate_employee(small_low, small_high, med_low, med_high, large_low, large_high):
     employee_small = Polygon()
     employee_small.add(x=0, y=0)
+    small_slop = max(small_low - 5, 0)
+
+    employee_small.add(x=small_low - small_slop , y = 1)
     employee_small.add(x=small_low, y = 1)
     employee_small.add(x=small_high, y = 1)
-    employee_small.add(x=med_low+5, y = 0)
+    employee_small.add(x=small_high + 2, y = 0)
 
     employee_med = Polygon()
     employee_med.add(x=med_low - 10, y = 0)
@@ -67,7 +70,6 @@ def generate_employee(small_low, small_high, med_low, med_high, large_low, large
     employee_large.add(x=large_low - 25, y = 0 )
     employee_large.add(x=large_low, y = 1 )
     employee_large.add(x=large_high, y = 1 )
-    employee_large.add(x=large_high + 50, y = 0 )
 
     return {
         "small": employee_small,
