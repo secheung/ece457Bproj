f260fd7feee4cde8de84dcb30cf6e191e6513bae
Junwin Li <junwin.li@gmail.com>
2015-03-21 00:00:35 -0400

    Add dot output

--- main.py
+++ main.py
@@ -58,22 +58,45 @@ def generateDocs(FuzzyController):
     # doc.create3DPlot(system,"input_rep","input_pay","happiness")
     # doc.create3DPlot(system,"input_employees","input_pay","happiness")
 
+def generateDot(FuzzyController):
+    system = FuzzyController.getFuzzySystem()
+    from fuzzy.doc.structure.dot import dot
+    import subprocess
+    # expects dot directory
+    directory = "dot"
+    for name,rule in system.rules.items():
+        cmd = "dot -T png -o '%s/Rule %s.png'" % (directory,name)
+        proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        dot.print_header(proc.stdin)
+        dot.print_dot(rule, proc.stdin, system, "")
+        dot.print_footer(proc.stdin)
+        proc.communicate()
+    cmd = "dot -Tpng -o '%s/System.png'" % directory
+    proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    dot.printDot(system, proc.stdin)
+    proc.communicate()
+
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"hp",["plot"])
+        opts, args = getopt.getopt(argv,"hpd",["plot","dot"])
     except getopt.GetoptError:
         print 'main.py -p'
         sys.exit(2)
 
     controller = Controller(users[0]);
 
+    exit = False
     for opt, arg in opts:
         if opt == '-h':
             print 'main.py -p'
-            sys.exit()
+            exit = True
         elif opt in ("-p", "--plot"):
             generateDocs(controller)
-            sys.exit(0)
+            exit = True
+        elif opt in ("-d", "--dot"):
+            generateDot(controller)
+            exit = True
+    if exit: sys.exit(0)
 
     for company in companies:
         res = controller.calculate(company["employees"], company["salary"], company["reputation"])
